30d746768ee5a384fee482b961dfdaac073b7fa0
Josafajosina <josafajosina@gmail.com>
2022-07-06 22:05:38 -0300

    Adicionando o estilo da página do jogo

--- Game/jogo/src/componente/Game.js
+++ Game/jogo/src/componente/Game.js
@@ -2,10 +2,31 @@ import './Game.css';
 
 const Game = ({verificarLetras}) => {
   return (
-    <div>
-      <h1>game</h1>
-      <button className='button' onClick={verificarLetras}>Finalizar</button>
+  <div className="game">
+    <p className="pontos">
+      <span>Pontos: 000 </span>
+    </p>
+    <h2>Adivinhe a Palavra</h2>
+    <h3 className="tip">
+      Dica da palavra: <span> Dica...</span>
+    </h3>
+    <div className="palavraContainer">
+      <span className="letras">A</span>
+      <span className="blankSquare"></span>
     </div>
+    <div className="letrasContainer">
+      <p>Adivinhe a palavras</p>
+      <form>
+      <input type="text" name='letter' maxLength='1' required/>
+      <button className="button">Jogar</button>
+      </form>
+    </div>
+    <div className="wrongLettersContainer">
+      <p>letras já utilizadas: </p>
+      <span>a, </span>
+      <span>b, </span>
+    </div>
+  </div>
   )
 }
 
